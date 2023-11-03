e1022c51a0422a6e3ed06893e9fb1602e45c1fdb
neehu <neehu@BATWOMAN>
2023-11-03 08:59:57 +0530

    Add project files.

---
+++ ConsoleApp1/GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GreatestCommonDivisorOfStrings
+    {
+        public static string GcdOfStrings(string str1, string str2)
+        {
+           
+           if(str1 + str2 != str1 + str2)
+            {
+                return "";
+            }
+            var n1 = str1.Length;
+            var n2 = str2.Length;
+            while (n1 != 0 && n2!= 0)
+            {
+                if(n1 > n2)
+                {
+                    n1 %= n2;
+                }
+                else
+                {
+                    n2 %= n1;
+                }
+            }
+            return str1.Substring(0, n1 | n2);
+        }
+    }
+}
